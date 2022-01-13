98b2e1d80c8a6f258900f467896d21b0499c33de
Myron David <myrondavid96@gmail.com>
2022-01-13 14:00:42 -0300

    Arrumando o acesso ao bd no server

--- Server/Program.cs
+++ Server/Program.cs
@@ -32,8 +32,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
 
