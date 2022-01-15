4c1dbee39f89a9ff2937934b8aa7fce6101c45c2
Myron David <myrondavid96@gmail.com>
2022-01-15 13:32:02 -0300

    Atualizando o endpoint da API no cliente

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -5,7 +5,7 @@
 #if DEBUG
         internal const string ServerBaseUrl = "https://localhost:5003";
 #else
-        internal const string ServerBaseUrl = "";
+        internal const string ServerBaseUrl = "https://cursoblazor.azurewebsites.net";
 #endif
         internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
     }
